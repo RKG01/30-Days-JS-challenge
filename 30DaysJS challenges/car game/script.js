95844f71609b4ff4853970f677674bc024012fd5
rahul <rk5849193@gmail.com>
2025-06-13 15:42:56 +0530

    30 Days js challenge Day1

---
+++ 30DaysJS challenges/car game/script.js
@@ -0,0 +1,128 @@
+const roadArea = document.querySelector('.road');
+let player = { step: 5 };
+let keys = { ArrowUp: false, ArrowDown: false, ArrowRight: false, ArrowLeft: false, Space: false };
+let score = 0;
+const startBtn = document.querySelector(".btn");
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
+function keyDown(event) {
+    if (event.key === " ") {
+        event.preventDefault(); 
+        keys.Space = true;
+    } else {
+        keys[event.key] = true;
+    }
+} i
+
+function keyUp(event) {
+    if (event.key === " ") {
+        keys.Space = false;
+    } else {
+        keys[event.key] = false;
+    }
+}
+
+function moveLines() {
+    let lines = document.querySelectorAll('.lines');
+    lines.forEach(item => {
+        if (item.y >= 700) {
+            item.y = item.y - 750;
+        }
+        item.y = item.y + player.step;
+        item.style.top = item.y + 'px';
+    });
+}
+
+function moveEnemies(playercar) {
+    let vehicles = document.querySelectorAll('.enemies');
+    let playercarb = playercar.getBoundingClientRect();
+
+    vehicles.forEach(item => {
+        let othercarb = item.getBoundingClientRect();
+        if (!((playercarb.bottom < othercarb.top) || (playercarb.top > othercarb.bottom) || (playercarb.left > othercarb.right) || (playercarb.right < othercarb.left))) {
+            alert("The Final Score is " + (score) + "\n Press OK to play again");
+            location.reload();
+            player.start = false;
+        }
+        if (item.y >= 815) {
+            score = score + 1;
+            startBtn.innerHTML = "Score: " + score;
+            item.y = -300;
+            item.style.left = Math.floor(Math.random() * 350) + 'px';
+        }
+        item.y = item.y + player.step;
+        item.style.top = item.y + 'px';
+    });
+}
+
+function playArea() {
+    let playercar = document.querySelector('.car');
+    let road = roadArea.getBoundingClientRect();
+    if (player.start) {
+        moveLines();
+        moveEnemies(playercar);
+        if (keys.ArrowUp && player.y > (road.top + 80)) {
+            player.y = player.y - player.step;
+        }
+
+        if (keys.ArrowDown && player.y < (road.bottom - 150)) {
+            player.y = player.y + player.step;
+        }
+
+        if (keys.ArrowLeft && player.x > 0) {
+            player.x = player.x - player.step;
+        }
+
+        if (keys.ArrowRight && player.x < (road.width - 64)) {
+            player.x = player.x + player.step;
+        }
+
+        if (keys.Space) {
+            console.log("Space key pressed");
+        }
+
+        playercar.style.top = player.y + 'px';
+        playercar.style.left = player.x + 'px';
+        window.requestAnimationFrame(playArea);
+    }
+}
+
+function init() {
+    player.start = true;
+    window.requestAnimationFrame(playArea);
+
+    for (let i = 0; i < 5; i++) {
+        let roadlines = document.createElement('div');
+        roadlines.setAttribute('class', 'lines');
+        roadlines.y = i * 150;
+
+        roadlines.style.top = roadlines.y + 'px';
+        roadArea.appendChild(roadlines);
+    }
+
+    let playercar = document.createElement('div');
+    playercar.setAttribute('class', 'car');
+    roadArea.appendChild(playercar);
+
+    player.x = playercar.offsetLeft;
+    player.y = playercar.offsetTop;
+
+    for (let x = 0; x < 4; x++) {
+        let enemies = document.createElement('div');
+        enemies.setAttribute('class', 'enemies');
+        enemies.y = ((x + 1) * 350) * -1;
+        enemies.style.top = enemies.y + 'px';
+
+        enemies.style.left = Math.floor(Math.random() * 350) + 'px';
+        roadArea.appendChild(enemies);
+    }
+}
+
+function startgame() {
+    document.querySelector(".push").style.border = "5px solid rgb(86,50,57)";
+    startBtn.style.backgroundColor = "rgb(86,50,57)";
+    startBtn.style.cursor = "not-allowed";
+    startBtn.innerHTML = "Use Arrow keys to Navigate";
+    init();
+}
